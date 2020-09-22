ca9f62aa48a4830032976fb4d2f06a6ede602f91
Pedro Santos <pedrosantos0172@gmail.com>
2020-09-22 08:50:25 +0100

    Initial Commit

---
+++ DivideTwoIntegers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DivideTwoIntegers
+//
+//  Created by Pedro Santos on 22/09/2020.
+//  Copyright © 2020 Pedro Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
